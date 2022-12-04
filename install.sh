2c6cca9074cabbea2ece626a2764ad490f0dc0f0
smurfdaddy <smurfdaddy@gmx.com>
2022-12-04 11:31:41 +0000

    simple installer script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DEST=/opt/google-chrome-the-latest
+mkdir -p $DEST
+chmod +x google-chrome-the-latest.py
+chmod +x google-chrome-the-latest-cron.sh
+
+cp google-chrome-the-latest.py $DEST
+cp google-chrome-the-latest-cron.sh $DEST
+cp permission-dialog.glade $DEST
+cp end-dialog.glade $DEST
+cp google-chrome-the-latest.png $DEST
+cp whatismybrowser-logo.png $DEST
+cp README $DEST
+
+cp google-chrome-the-latest-cron.sh /etc/cron.hourly
