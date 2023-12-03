ac8e988467643e1bfe6e32a22249c1cf32665692
Antonio Leal <antonio.leal@yahoo.com>
2023-12-03 00:16:34 +0000

    installer update

--- install.sh
+++ install.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR
+
 DEST=/opt/google-chrome-the-latest
 mkdir -p $DEST
-chmod +x google-chrome-the-latest.py
-chmod +x google-chrome-the-latest-cron.sh
 
-cp google-chrome-the-latest.py $DEST
-cp google-chrome-the-latest-cron.sh $DEST
-cp permission-dialog.glade $DEST
-cp end-dialog.glade $DEST
-cp google-chrome-the-latest.png $DEST
-cp whatismybrowser-logo.png $DEST
-cp README $DEST
-cp INSTALL $DEST
-cp LICENSE $DEST
-cp install.sh $DEST
-cp uninstall.sh $DEST
+chmod 644 *
+chmod +x *.py
+chmod +x *.sh
 
+cp * $DEST
+
+cp google-chrome-the-latest.png /usr/share/pixmaps
+cp google-chrome-the-latest.desktop /usr/share/applications
 cp google-chrome-the-latest-cron.sh /etc/cron.hourly
